4661a67df343240506d1235ff0b98c2bf77989ef
Chajac <54216608+Chajac@users.noreply.github.com>
2023-07-22 18:31:25 +0100

    Updated variable for legibility

--- src/bayerdither.tsx
+++ src/bayerdither.tsx
@@ -1,3 +1,47 @@
-import THREE, { Uniform, Vector3 } from "three";
+import { Uniform } from "three";
 import { Effect } from "postprocessing";
 import bayerdither from "./bayer.glsl";
+
+export interface IDitherProps {
+	ditherScale?: number;
+	colorThreshold?: number;
+	matrixSize?: number;
+	dOX?: number;
+	dOY?: number;
+	xDarkThreshold?: number;
+	yWhiteThreshold?: number;
+	weight?: number;
+	randomize?: boolean;
+	seed?: number;
+}
+
+// Effect implementation
+class DitherEffect extends Effect {
+	constructor({
+		ditherScale = 1,
+		matrixSize = 4,
+		colorThreshold = 1024,
+		dOX = 0,
+		dOY = 0,
+		xDarkThreshold = 0,
+		yWhiteThreshold = 0,
+		randomize = false,
+		seed = 0,
+	}: IDitherProps = {}) {
+		const uniforms = new Map<string, Uniform>([
+			["uDitherScale", new Uniform(ditherScale)],
+			["uColorThreshold", new Uniform(colorThreshold)],
+			["uMatrixSize", new Uniform(matrixSize)],
+			["uDitherOffsetX", new Uniform(dOX)],
+			["uDitherOffsetY", new Uniform(dOY)],
+			["uDarkThreshold", new Uniform(xDarkThreshold)],
+			["uWhiteThreshold", new Uniform(yWhiteThreshold)],
+			["uRandomize", new Uniform(randomize)],
+			["uSeed", new Uniform(seed)],
+		]);
+
+		super("BeyerDither", bayerdither, {
+			uniforms,
+		});
+	}
+}
