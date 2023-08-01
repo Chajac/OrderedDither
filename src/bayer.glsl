60c3c18427b3cdbc30875c5d0802218173564b05
Chajac <54216608+Chajac@users.noreply.github.com>
2023-08-01 02:00:20 +0100

    Variable renaming isn't daft now.

--- src/bayer.glsl
+++ src/bayer.glsl
@@ -1,17 +1,13 @@
-uniform sampler2D tDiffuse;
-uniform float uWeight;
+
 uniform float uDitherScale;
-uniform float uDitherIntensity;
+uniform float uColorThreshold;
 uniform int uMatrixSize;
-uniform float uDitherX;
-uniform float uDitherY;
-uniform float uXAmount;
-uniform float uYAmount;
+uniform float uDitherOffsetX;
+uniform float uDitherOffsetY;
+uniform float uDarkThreshold;
+uniform float uLightThreshold;
 uniform float uSeed;
 uniform bool uRandomize;
-
-
-vec2 uResolution = vec2(1200,1200);
 
 
 float rand(vec2 co) {
@@ -26,11 +22,6 @@ const mat2 bayer2x2 = mat2(1.0, 3.0, 4.0, 2.0) / 4.0;
 const mat3 bayer3x3 = mat3(0.0, 7.0, 3.0,
     6.0, 5.0, 2.0,
     4.0, 1.0, 8.0) / 9.0;
-
-//const mat4 bayer4x4 = mat4(0.0, 9.0, 3, 11.0,
-//        13.0, 5.0, 15.0, 7.0,
-//        4.0, 12.0, 2.0, 10.0,
-//        16.0, 8.0, 14.0, 6.0) / 16.0;   
 
 
 const mat4 bayer4x4 = mat4(0.,  8.,  2., 10.,
@@ -123,10 +114,6 @@ float getGrayscale(vec4 color) {
     return dot(color.rgb, wG);
 }
 
-
-float contrast =  0.0;
-float epsilon = 0.001;
-
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
 
     
@@ -134,60 +121,59 @@ float RECIPROCAL_MATRIX_SIZE = 1.0 / float(uMatrixSize * uMatrixSize);
 
     float dither;
     vec2 ditheredCoord = floor((uv * resolution) / uDitherScale);
-    //vec2 positionInMatrix = mod(floor(uv * resolution.xy), 3.0);
 
     if(uRandomize == false){
         if (uMatrixSize == 2) {
             mat2 matrix = randomMatrix2x2(uSeed);
-            dither = matrix[int(mod(ditheredCoord.y, (2.0 + uDitherY )))][int(mod(ditheredCoord.x, (2.0 + uDitherX)))] + RECIPROCAL_MATRIX_SIZE * uXAmount;
+            dither = matrix[int(mod(ditheredCoord.y, (2.0 + uDitherOffsetY )))][int(mod(ditheredCoord.x, (2.0 + uDitherOffsetX)))] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold;
             //dither = randomMatrix2x2[int(ditheredCoord.x)%4][int(ditheredCoord.y)%4] ;
         } else if (uMatrixSize == 3) {
 
             mat3 matrix = randomMatrix3x3(uSeed);
-            dither = matrix[int(mod(ditheredCoord.y, (3.0 + uDitherY )))][int(mod(ditheredCoord.x, (3.0 + uDitherX)))] + RECIPROCAL_MATRIX_SIZE * uXAmount;
+            dither = matrix[int(mod(ditheredCoord.y, (3.0 + uDitherOffsetY )))][int(mod(ditheredCoord.x, (3.0 + uDitherOffsetX)))] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold;
             //dither = randomMatrix2x2[int(ditheredCoord.x)%4][int(ditheredCoord.y)%4] ;
         } else if (uMatrixSize == 4){
             mat4 matrix = randomMatrix4x4(uSeed);
-            dither = matrix[int(mod(ditheredCoord.y, (4.0 + uDitherY )))][int(mod(ditheredCoord.x, (4.0 + uDitherX)))] + RECIPROCAL_MATRIX_SIZE * uXAmount;
+            dither = matrix[int(mod(ditheredCoord.y, (4.0 + uDitherOffsetY )))][int(mod(ditheredCoord.x, (4.0 + uDitherOffsetX)))] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold;
 
 
         } else if (uMatrixSize == 8){
             generateRandomMatrix8x8(uSeed);
-            int row = int(mod(ditheredCoord.x, (8.0 + uDitherX)));
-            int col = int(mod(ditheredCoord.y, (8.0 + uDitherY)));
-            dither = (randomMatrix8x8[row * 8 + col] + RECIPROCAL_MATRIX_SIZE * uXAmount);
+            int row = int(mod(ditheredCoord.x, (8.0 + uDitherOffsetX)));
+            int col = int(mod(ditheredCoord.y, (8.0 + uDitherOffsetY)));
+            dither = (randomMatrix8x8[row * 8 + col] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold);
         }{
             inputColor;
         }
 
     }else if(uRandomize == true)  {
  if (uMatrixSize == 2) {
-         dither = bayer2x2[int(mod(ditheredCoord.y, (2.0 + uDitherY)))][int(mod(ditheredCoord.x, (2.0 + uDitherX)))] + RECIPROCAL_MATRIX_SIZE * uXAmount;
+         dither = bayer2x2[int(mod(ditheredCoord.y, (2.0 + uDitherOffsetY)))][int(mod(ditheredCoord.x, (2.0 + uDitherOffsetX)))] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold;
          
      } else if (uMatrixSize == 3) {
-        dither = bayer3x3[int(mod(ditheredCoord.y, (3.0 + uDitherY )))][int(mod(ditheredCoord.x, (3.0 + uDitherX)))] + RECIPROCAL_MATRIX_SIZE * uXAmount;
+        dither = bayer3x3[int(mod(ditheredCoord.y, (3.0 + uDitherOffsetY )))][int(mod(ditheredCoord.x, (3.0 + uDitherOffsetX)))] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold;
      } else if (uMatrixSize == 4) {
 
-         dither = bayer4x4[int(mod(ditheredCoord.y, (4.0 + uDitherY)))][int(mod(ditheredCoord.x, (4.0 + uDitherX) ))] + RECIPROCAL_MATRIX_SIZE * uXAmount;
+         dither = bayer4x4[int(mod(ditheredCoord.y, (4.0 + uDitherOffsetY)))][int(mod(ditheredCoord.x, (4.0 + uDitherOffsetX) ))] + RECIPROCAL_MATRIX_SIZE * uDarkThreshold;
      } else if (uMatrixSize == 8) {
 
         //Cannot use the same method as above; must use 1D array to calculate index
-        int row = int(mod(ditheredCoord.x, (8.0 + uDitherX)));
-        int col = int(mod(ditheredCoord.y, (8.0 + uDitherY)));
-        dither = (bayer8x8[row * 8 + col] + RECIPROCAL_MATRIX_SIZE *  (mix(0., uXAmount * 50.,2.3))) / 64.0;
+        int row = int(mod(ditheredCoord.x, (8.0 + uDitherOffsetX)));
+        int col = int(mod(ditheredCoord.y, (8.0 + uDitherOffsetY)));
+        dither = (bayer8x8[row * 8 + col] + RECIPROCAL_MATRIX_SIZE *  (mix(0., uDarkThreshold * 50.,2.3))) / 64.0;
      } {
          inputColor;
      }
     }
  
-    float steps = uDitherIntensity;
-    vec4 quantizedColor = vec4(vec3(floor(inputColor.rgb * steps)/steps), inputColor.a);
+
+    vec4 quantizedColor = vec4(vec3(floor(inputColor.rgb * uColorThreshold)/uColorThreshold), inputColor.a);
     float grayscale = getGrayscale(quantizedColor);
 
-    float quantDither = floor((grayscale) * (1.1 + uYAmount) + dither);
+    float quantDither = floor((grayscale) * (1.1 + uLightThreshold) + dither);
 
     
     //Greyscale output
     outputColor = vec4(vec3(quantDither),1.0);
     //COLOR OUTPUT
-    
+   //outputColor = vec4(quantizedColor.rgb * quantDither, inputColor.a);
