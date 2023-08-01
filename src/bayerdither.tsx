b70e759164d5e7733bc6fa646be94acbed7ee53e
Chajac <54216608+Chajac@users.noreply.github.com>
2023-08-01 02:02:30 +0100

    Clean up and variable renaming

--- src/bayerdither.tsx
+++ src/bayerdither.tsx
@@ -6,38 +6,40 @@ export interface IDitherProps {
 	ditherScale?: number;
 	colorThreshold?: number;
 	matrixSize?: number;
-	dOX?: number;
-	dOY?: number;
+	dOffsetX?: number;
+	dOffsetY?: number;
 	xDarkThreshold?: number;
 	yWhiteThreshold?: number;
-	weight?: number;
 	randomize?: boolean;
 	seed?: number;
+	useColor?: boolean;
 }
 
 // Effect implementation
-class DitherEffect extends Effect {
+export class DitherEffect extends Effect {
 	constructor({
 		ditherScale = 1,
 		matrixSize = 4,
 		colorThreshold = 1024,
-		dOX = 0,
-		dOY = 0,
+		dOffsetX = 0,
+		dOffsetY = 0,
 		xDarkThreshold = 0,
 		yWhiteThreshold = 0,
 		randomize = false,
 		seed = 0,
+		useColor = false,
 	}: IDitherProps = {}) {
 		const uniforms = new Map<string, Uniform>([
 			["uDitherScale", new Uniform(ditherScale)],
 			["uColorThreshold", new Uniform(colorThreshold)],
 			["uMatrixSize", new Uniform(matrixSize)],
-			["uDitherOffsetX", new Uniform(dOX)],
-			["uDitherOffsetY", new Uniform(dOY)],
+			["uDitherOffsetX", new Uniform(dOffsetX)],
+			["uDitherOffsetY", new Uniform(dOffsetY)],
 			["uDarkThreshold", new Uniform(xDarkThreshold)],
 			["uWhiteThreshold", new Uniform(yWhiteThreshold)],
 			["uRandomize", new Uniform(randomize)],
 			["uSeed", new Uniform(seed)],
+			["uUseColor", new Uniform(useColor)],
 		]);
 
 		super("BeyerDither", bayerdither, {
